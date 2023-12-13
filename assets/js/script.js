dad8b52f857309e74c50d5083837fc90e02863bd
Carlos Pinheiro <dev.carloseps@gmail.com>
2023-12-13 14:48:49 -0300

    Updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
-const numeroSorteado = Math.floor(Math.random() * 51);
+const numeroSorteado = Math.floor(Math.random() * 1001);
+
+const tentativas = 0;
 
 function verificarPalpite() {
     const palpiteUsuario = parseInt(document.getElementById('userGuess').value);
 
-    if (isNaN(palpiteUsuario) || palpiteUsuario < 0 || palpiteUsuario > 50) {
-        alert("Por favor, insira um número válido entre 0 e 50.");
+    if (isNaN(palpiteUsuario) || palpiteUsuario < 0 || palpiteUsuario > 1000) {
+        alert("Por favor, insira um número válido entre 0 e 1000.");
     } else {
         if (palpiteUsuario === numeroSorteado) {
             document.getElementById('resultado').innerHTML = `Parabéns! Você acertou! O número sorteado é ${numeroSorteado}.`;
         } else if (palpiteUsuario < numeroSorteado) {
             document.getElementById('resultado').innerHTML = 'Tente novamente. O número é maior.';
+            ++tentativas;
         } else {
             document.getElementById('resultado').innerHTML = 'Tente novamente. O número é menor.';
+            ++tentativas;
         }
     }
 }
